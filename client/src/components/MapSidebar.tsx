c96486e901ee97e3f03461d54e957423c5b798bc
[Kyle Shi] <kyleshi88@gmail.com>
2025-10-14 00:03:18 -0400

    Fixed more issues from MapSidebar regarding SVG

--- client/src/components/MapSidebar.tsx
+++ client/src/components/MapSidebar.tsx
@@ -101,16 +101,16 @@ const MapSidebar: React.FC<MySidebarProps> = (props) => {
         setIsAuthenticated(false);
         setToken("");
     }
-    const addSymbol = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round" className="icon-plus">
+    const addSymbol = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon-plus">
             <path d="M12 5v14M5 12h14"/>
         </svg>
-    const trashSymbol = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round" className="icon-trash">
+    const trashSymbol = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon-trash">
             <path d="M3 6h18"/>
             <path d="M19 6l-1 14H6L5 6"/>
             <path d="M9 6V4h6v2"/>
             <path d="M10 11v6M14 11v6"/>
         </svg>
-    const logoutSymbol = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round" className="icon-logout">
+    const logoutSymbol = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon-logout">
         <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/>
         <path d="M16 17l5-5-5-5"/>
         <path d="M21 12H9"/>
