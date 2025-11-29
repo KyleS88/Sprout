20098098e84ab4e2fdf880d164479b1d6ead3d9b
[Kyle Shi] <kyleshi88@gmail.com>
2025-11-29 00:30:41 -0500

    Reupdated with connector prefix

--- server/Controller/user.controller.ts
+++ server/Controller/user.controller.ts
@@ -19,7 +19,7 @@ export const register = async (req: Request, res: Response) => {
             return res.status(400).json({
                 message: "Please provide all user details",
             });}
-        const user: QueryResultRow = await pool.query("SELECT * FROM users WHERE email = $1 OR username = $2;", [email, username, ]);
+        const user: QueryResultRow = await pool.query("SELECT * FROM connector.users WHERE email = $1 OR username = $2;", [email, username, ]);
         if (user.rows.length > 0) {
             const existingUser: UserDB = user.rows[0];
             if (existingUser.email === email) {
@@ -42,7 +42,7 @@ export const register = async (req: Request, res: Response) => {
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
-        const query: string = `INSERT INTO users (username, email, password, user_id) VALUES ($1, $2, $3, $4);`
+        const query: string = `INSERT INTO connector.users (username, email, password, user_id) VALUES ($1, $2, $3, $4);`
         await pool.query(query, [username, email, hashedPassword, uuid()])
         return res.status(200).json({
             message: "User registered succesfully",
@@ -63,7 +63,7 @@ export const login = async (req: Request, res: Response) => {
                 message: "Please provide all user details",
             });
         }
-        const user = await pool.query("SELECT * FROM users WHERE email=$1;", [email,]);
+        const user = await pool.query("SELECT * FROM connector.users WHERE email=$1;", [email,]);
         if (user.rows.length === 0) {
             return res.status(400).json({
                 message: `There is no user with the email: ${email}`,
